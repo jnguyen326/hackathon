b1ee0fa708d3d7f50c83734954f56c0343434b25
Jasmine Nguyen <jnguyen1941@sdsu.edu>
2023-11-01 16:51:41 -0700

    yo?

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from google.cloud import storage
-from google.cloud import aiplatform
-import PyPDF2
-from io import BytesIO
 import vertexai
 from vertexai.language_models import TextGenerationModel
+from dotenv import load_dotenv
+import configparser
+import PyPDF2
+from io import BytesIO
+
+load_dotenv()
+
+# Getter config variables
+config = configparser.ConfigParser()
+config.read('.config')
+GCS_BUCKET_NAME = config['DEFAULT']['GCS_BUCKET_NAME']
+SERVICE_ACCOUNT_PATH = config['DEFAULT']['SERVICE_ACCOUNT_PATH']
+SECRET_KEY = config['DEFAULT']['SECRET_KEY']
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # for flash messaging
+app.secret_key = SECRET_KEY
 
 # Set up Google Cloud Storage
-GCS_BUCKET_NAME = "cloud-ai-platform-5b21884c-0af6-4620-9a37-b1d5f8588f35"
-storage_client = storage.Client.from_service_account_json("C:\\Users\Owner\\PycharmProjects\\hackathon\\calm-brookkey.json")
+storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_PATH)
 bucket = storage_client.bucket(GCS_BUCKET_NAME)
 
 # Initialize vertexai
@@ -26,34 +35,34 @@ parameters = {
 }
 model = TextGenerationModel.from_pretrained("text-bison")
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Upload to GCS
-        pdf = request.files["file"]
-        blob = bucket.blob(pdf.filename)
-        blob.upload_from_file(pdf)
-
         # Extract text from PDF
+        pdf = request.files["file"]
         pdf_data = pdf.read()
+        if not pdf_data:
+            return "Uploaded file is empty. Please upload a valid PDF."
+
         pdf_file = BytesIO(pdf_data)
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+
+        # Upload to GCS
+        blob = bucket.blob(pdf.filename)
+        blob.upload_from_file(pdf_file)  # We're uploading pdf_file, not pdf
+
+        # Process the PDF content
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
         text = ""
-        for page_num in range(pdf_reader.numPages):
-            text += pdf_reader.getPage(page_num).extractText()
+        for page in pdf_reader.pages:  # Iterating directly over pages
+            text += page.extract_text()  # Changed this line to use the new method
 
         # Feed the extracted text to vertexai model
         response = model.predict(text, **parameters)
 
-        # Optionally, you can send the vertexai response to another Vertex AI endpoint or handle it differently.
-        # For this example, we'll flash the response
         flash(f"Design evaluated: {response.text}")
-
         return redirect(url_for("index"))
 
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
