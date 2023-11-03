79e4ae6d624de55ede7b552c35e5660fc51aaf50
Jasmine Nguyen <jnguyen1941@sdsu.edu>
2023-11-03 08:41:47 -0700

    jasmine coded so much she wants to curl up

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from google.cloud import storage
 from dotenv import load_dotenv
@@ -7,30 +8,18 @@ from langchain.document_loaders import PyPDFLoader
 import vertexai
 import os
 import tempfile
-from transformers import CLIPProcessor, CLIPModel
+from pypdf import PdfReader
+from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
+import logging
 
 load_dotenv()
-
-processor = None
-model = None
-
-
-def get_image_caption(image_path):
-    global processor, model
-    if processor is None or model is None:
-        processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch16")
-        model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
-
-    image = Image.open(image_path)
-    inputs = processor(text=["a photo of a [MASK]"], images=image, return_tensors="pt", padding=True)
-    outputs = model(**inputs)
-    logits_per_image = outputs.logits_per_image
-    probs = logits_per_image.softmax(dim=1)
-    caption = processor.decode(probs.argmax(dim=1).tolist()[0])
-    return caption.replace('[MASK]', '').strip()
+# Load pre-trained model and processor
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 
+# Load your configurations
 config = configparser.ConfigParser()
 config.read('.config')
 GCS_BUCKET_NAME = config['DEFAULT']['GCS_BUCKET_NAME']
@@ -40,12 +29,17 @@ SECRET_KEY = config['DEFAULT']['SECRET_KEY']
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+# Configure Flask logging
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
 storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_PATH)
 bucket = storage_client.bucket(GCS_BUCKET_NAME)
 
 vertexai.init(project="calm-brook-403808", location="us-central1")
-model = TextGenerationModel.from_pretrained("text-bison@001")
-
+text_generation_model = TextGenerationModel.from_pretrained("text-bison")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -64,6 +58,7 @@ def index():
         pdf_file_path = temp_file.name
         temp_file.close()
 
+        # Upload to Google Cloud Storage
         blob = bucket.blob(pdf.filename)
         blob.content_type = 'application/pdf'  # Explicitly set the MIME type
         with open(pdf_file_path, "rb") as my_file:
@@ -73,31 +68,57 @@ def index():
         blob.patch()
         pdf_url = blob.public_url
 
+        # Load PDF using PyPDFLoader
         loader = PyPDFLoader(pdf_file_path)
+        documents = loader.load()  # This will give us the list of Document objects
+        num_pages = len(documents)
+        app.logger.info(f"PDF loaded with {num_pages} pages.")
 
+        def get_image_caption(image_path):
+            # Load and process the image
+            image = Image.open(image_path)
+            inputs = processor(images=image, return_tensors="pt")
 
-        data = loader.load()
-        os.remove(pdf_file_path)
+            # Generate caption
+            outputs = model.generate(**inputs, max_length=128, num_beams=3)
 
+            # Convert token ids to text
+            captions = processor.decode(outputs[0], skip_special_tokens=True)
+            return captions
+
+        # Extract images and get captions
         captions = []
-        try:
-            extracted_images = loader.get_extracted_images()  # Assuming this method is available in PyPDFLoader
-            print(f"Number of images extracted: {len(extracted_images)}")
-            for image_path in extracted_images:
-                caption = get_image_caption(image_path)
-                print(f"Caption for {image_path}: {caption}")
-                captions.append(caption)
-                app.logger.info(f"Number of images extracted: {len(extracted_images)}")
-                app.logger.info(f"Caption for {image_path}: {caption}")
+        reader = PdfReader(pdf_file_path)
+        for page_num, page in enumerate(reader.pages):
+            count = 0
+            for image in page.images:
+                image_filename = f"image_{page_num}_{count}.jpg"
+                with open(image_filename, "wb") as fp:
+                    fp.write(image.data)
+                try:
+                    caption = get_image_caption(image_filename)
+                    captions.append(caption)
+                    # Log the caption for the current image
+                    app.logger.info(f"Caption for {image_filename}: {caption}")
+                    os.remove(image_filename)  # Clean up the image file after getting the caption
+                except Exception as e:
+                    app.logger.error(f"Failed to get caption for {image_filename}: {e}")
+                count += 1
 
+        # Assuming 'captions' is a list of strings extracted from the PDF
+        captions_text = "\n".join(captions)
 
-        except AttributeError:
-            # If the get_extracted_images method doesn't exist, this block will catch the error.
-            pass
+        # Construct the prompt with the dynamic captions included
+        prompt = f"""
+        You are an AI-powered UX/UI design evaluator. Your objective is to analyze UI/UX elements based on provided descriptions and offer comprehensive, insightful, and actionable feedback. Your feedback should help designers, businesses, and professionals refine their creations, focusing on both usability and aesthetics. Quality and clarity in your critique are essential.
 
+        Based on the following descriptions of images from a PDF:
 
-        all_captions = ' '.join(captions)
-        prompt = f'Can you please provide recommendations that would improve the UI/UX design of the PDF? These can be your thoughts regarding the flow of the document, visuals, colors, the content, or anything else you think of. Your objective is to analyze its elements and provide comprehensive, insightful, and actionable feedback. You will be fed the images and text information. You should assess both the usability and aesthetics of the design, offering suggestions for improvement while also highlighting strengths. Your feedback should aid designers, businesses, and professionals in refining and enhancing their creations. Remember, quality and clarity in your critique are essential. Can you also explain your rationale behind these changes. Thank you... {all_captions}'
+        {captions_text}
+
+        Please provide recommendations that would improve the UI/UX design of the content described. Consider the flow of information, font choices, color schemes, layout, and overall aesthetic appeal in your feedback. Highlight both strengths and areas for improvement, and explain your rationale behind each suggestion for enhancement. Thank you.
+        """
+
         parameters = {
             "candidate_count": 1,
             "max_output_tokens": 1024,
@@ -106,19 +127,35 @@ def index():
             "top_k": 40
         }
         try:
-            response = model.predict(prompt, **parameters)
+            response = text_generation_model.predict(prompt, **parameters)
+            print(f"Response from Model: {response.text}")
             initial_feedback = response.text
         except Exception as e:
             print(f"Error occurred: {e}")
             flash("Error occurred during evaluation. Please try again later.")
             return redirect(url_for("index"))
+        global design_context
+        design_context = captions_text
 
     return render_template("index.html", initial_feedback=initial_feedback, pdf_url=pdf_url)
 
+# Global variable to store the captions context
+design_context = ""
 
 @app.route("/ask", methods=["POST"])
 def ask():
     user_message = request.form["message"]
+
+    # Formulate the prompt with the design context and the user's message
+    prompt = f"""
+    You are an AI that has analyzed a design with the following details:
+
+    {design_context}
+
+    User's question: "{user_message}"
+    How would you respond to improve the design based on their query?
+    """
+
     parameters = {
         "candidate_count": 1,
         "max_output_tokens": 1024,
@@ -126,13 +163,13 @@ def ask():
         "top_p": 0.8,
         "top_k": 40
     }
+
     try:
-        response = model.predict(user_message, **parameters)
+        response = text_generation_model.predict(prompt, **parameters)
         return jsonify({"response": response.text})
     except Exception as e:
         print(f"Error occurred: {e}")
         return jsonify({"response": "Sorry, an error occurred. Please try again later."})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/assets/scripts.js
+++ static/assets/scripts.js
@@ -1,28 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Bind the submit event to the button
+    const chatLog = document.getElementById('chatLog');
+    const userInput = document.getElementById('userInput');
+
+    // Bind the Enter key to submit the question and bind the click event to the submit button
+    userInput.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            submitQuestion();
+        }
+    });
+
     const submitBtn = document.querySelector("button");
     if (submitBtn) {
         submitBtn.addEventListener("click", submitQuestion);
     }
-});
 
-const sidebar = document.querySelector('.sidebar');
-const navigatorBtn = document.querySelector('.navigator-button'); // You may need to adjust the selector if it's different
-
-navigatorBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
+    // Toggle sidebar
+    const sidebar = document.querySelector('.sidebar');
+    const navigatorBtn = document.querySelector('.navigator-button');
+    navigatorBtn.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+    });
 });
 
 function submitQuestion() {
-    const userInput = document.getElementById('userInput').value;
+    const userInput = document.getElementById('userInput');
+    const userText = userInput.value;
 
-    if (userInput.trim() === '') return;
+    if (userText.trim() === '') return;
 
-    // Add the user's message to the chat log
+    // Disable the button to prevent double submissions
+    document.querySelector("button").disabled = true;
+
+    // Add the user's message to the chat log with bounce animation
     const chatLog = document.getElementById('chatLog');
     const userMessage = document.createElement('p');
-    userMessage.innerHTML = "<strong>You:</strong> " + userInput;
+
+    userMessage.innerHTML = "<strong>You:</strong> " + userText;
+    userMessage.className = 'user animate__animated animate__bounceIn';
     chatLog.appendChild(userMessage);
+
+    // Scroll to the new message
+    userMessage.scrollIntoView();
+
+    // Clear the input field
+    userInput.value = '';
 
     // Send a request to the server to get the AI's response
     fetch('/ask', {
@@ -30,12 +52,22 @@ function submitQuestion() {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: 'message=' + encodeURIComponent(userInput),
+        body: 'message=' + encodeURIComponent(userText),
     })
     .then(response => response.json())
     .then(data => {
+        // Re-enable the button
+        document.querySelector("button").disabled = false;
+
         const aiMessage = document.createElement('p');
         aiMessage.innerHTML = "<strong>AI Evaluator:</strong> " + data.response;
+        userMessage.className = 'user animate__animated animate__bounceIn';
         chatLog.appendChild(aiMessage);
+
+        // Scroll to the AI's message
+        aiMessage.scrollIntoView();
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
 }
